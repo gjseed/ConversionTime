de2ccce5c61e82c22cd5904387f7b7c79152972c
glinda seed <glinda.seed@student.centralia.edu>
2020-05-18 16:26:34 -0700

    Forgot to commit

---
+++ ConversionTime/Pages/Mass.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConversionTime.Pages
+{
+    public class MassModel : PageModel
+    {
+        public int Po2Ou { get; set; }
+        public int Ou2Po { get; set; }
+        public int Gr2Po { get; set; }
+        public int Mg2Po { get; set; }
+
+        public void OnPost()
+        {
+            //var MassOpt = Request.Form["MassOpt"];
+            double convMass = 0;
+            if(MassOpt == "1")
+            {
+                convMass = convMass * 16;
+            }else if(MassOpt == "2")
+            {
+                convMass = convMass / 16;
+            }else if (MassOpt == "3")
+            {
+                convMass = convMass / 454;
+            }
+            else
+            {
+                convMass = convMass / 453592;
+            }
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ ConversionTime/Pages/Temperature.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConversionTime.Pages
+{
+    public class TemperatureModel : PageModel
+    {
+        [BindProperty]
+        public double FahtoCel { get; set; }
+        [BindProperty]
+        public double CeltoFah { get; set; }
+
+
+        public void OnPost()
+        {
+            //var TempOpt = Request.Form["TempOpt"];
+            double convertedTemp;
+            if (TempOpt == 1)
+            {
+                //ViewData["convertedTemp"] = (FahtoCel - 32) * 5 / 9;
+                convertedTemp = (FahtoCel - 32) * 5 / 9;
+
+            }
+            else
+            {
+                //ViewData["convertedTemp"] = (CeltoFah * 9 / 5) + 32;
+                convertedTemp = (CeltoFah * 9 / 5) + 32;
+
+            }
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
